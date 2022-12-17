29b49a9f8bf8ceccb71782804bef7539e2f612a4
Hadar5325 <hadar.r5325@gmail.com>
2022-12-17 17:29:02 +0200

    cleaning code

--- memeController.js
+++ memeController.js
@@ -16,18 +16,7 @@ const BLACK_COLOR = '#000000'
 
 settingMemes()
 
-gLines = [{
-    xStartLine0: 10,
-    yStartLine0: 50
-}, {
-    xStartLine1: 10,
-    yStartLine1: 350
-}, {
-    xstartLineN: 10,
-    xstartLineN: 200,
-}
-]
-const arr = { 0: 50, 1: 150, 2: 350 }
+const arr = { 0: 50, 1: 250, 2: 350 }
 
 function settingMemes() {
     gElCanvas = document.querySelector('canvas')
@@ -41,9 +30,9 @@ function settingMemes() {
         rightPos: gElCanvas.width - gElCanvas.width / 10
     }
     gheight = {
-        topLine: 0 + gElCanvas.height / 10,
-        middleLines: gElCanvas.height / 2,
-        bottomLine: gElCanvas.height - gElCanvas.height / 10
+        0: 0 + gElCanvas.height / 10,
+        1: gElCanvas.height / 2,
+        2: gElCanvas.height - gElCanvas.height / 10
     }
 }
 
@@ -76,18 +65,14 @@ function linesOnCanvas(lines, currMeme) {
     let alignPos
     switch (lines.length) {
         case 0:
-            console.log('linesOnCanvas -memecontroller 0')
             break;
         case 1:
-            console.log('linesOnCanvas -memecontroller 1 ')
             alignPos = getColorFontAndAlign(currMeme, 0)
             gCtx.fillText(lines[0], alignPos, arr[0])
             gCtx.strokeText(lines[0], alignPos, arr[0])
             break;
 
         case 2:
-            console.log('linesOnCanvas -memecontroller 2 ')
-
             alignPos = getColorFontAndAlign(currMeme, 0)
             gCtx.fillText(lines[0], alignPos, arr[0])
             gCtx.strokeText(lines[0], alignPos, arr[0])
@@ -99,8 +84,6 @@ function linesOnCanvas(lines, currMeme) {
 
             break;
         default:
-
-            console.log('linesOnCanvas -memecontroller defualt ')
 
             alignPos = getColorFontAndAlign(currMeme, 0)
             gCtx.fillText(lines[0], alignPos, arr[0])
@@ -114,7 +97,6 @@ function linesOnCanvas(lines, currMeme) {
             for (var i = 0; i < lines.length; i++) {
                 if (i === 0 || i === 1) continue
                 alignPos = getColorFontAndAlign(currMeme, i)
-                console.log(lines[i], alignPos, arr[1])
                 gCtx.fillText(lines[i], alignPos, arr[1])
                 gCtx.strokeText(lines[i], alignPos, arr[1]);
             }
@@ -171,7 +153,6 @@ function lineOfTextOnCanvas(linesToShow) {
 
             const mesureCase1 = gCtx.measureText(linesToShow[0]).width
             updateGmemeMesures(mesureCase1)
-            console.log(gCtx.measureText(linesToShow))
             break;
 
 
@@ -224,7 +205,7 @@ function onIncreaseFont() {
     gCtx.font = `${numOfFont}px serif`;
 }
 
-function onChangeFont(value) {    
+function onChangeFont(value) {
     const symbol = value.innerText.includes('+')
 
     const arr = gCtx.font.split('px')
@@ -256,11 +237,8 @@ function checkChoosingFontBeforeOpeningLineData(font) {
 //Change color of text 
 var elInputColor = document.querySelector('.colorInput')
 elInputColor.addEventListener('input', function () {
-    console.log('input color!')
     const elInputLine = document.querySelector('.input-line')
-    // console.log(elInputLine.value ===)
     const color = elInputColor.value;
-    console.log(color)
     if (checkChoosingColorBeforeOpeningLineData(elInputLine, color)) return
 
     updateColor(color)
@@ -270,18 +248,18 @@ elInputColor.addEventListener('input', function () {
 }, false);
 
 
-function showAndHiddenInputColorFont(){
-   const elPaint = document.querySelector('.paint-icon')
-   elPaint.classList.add('.shown')
+function showAndHiddenInputColorFont() {
+    const elPaint = document.querySelector('.paint-icon')
+    elPaint.classList.add('.shown')
 
-   const elInput = document.querySelector('.hidden-icon')
-   elInput.classList.remove('.hidden-icon-color')
+    const elInput = document.querySelector('.hidden-icon')
+    elInput.classList.remove('.hidden-icon-color')
 }
 
-function showAndHiddenInputStrokeFont(){
+function showAndHiddenInputStrokeFont() {
     const elStroke = document.querySelector('.stroke-icon')
     elStroke.classList.add('.shown')
- 
+
     const elInput = document.querySelector('.hidden-icon')
     elInput.classList.remove('hidden-icon-storoke')
 }
@@ -289,7 +267,6 @@ function showAndHiddenInputStrokeFont(){
 function checkChoosingColorBeforeOpeningLineData(elInput, color) {
     if (elInput.value === '' && elInput.getAttribute('placeholder') === 'Text line') {
         gPickedColor = color
-        console.log(gPickedColor, "here!")
         return true
     }
     return false
@@ -304,7 +281,6 @@ function checkChoosingColorBeforeOpeningLineData(elInput, color) {
 var elInputStrokeColor = document.querySelector('.strokeColorInput')
 elInputStrokeColor.addEventListener('input', function () {
     const elInputLine = document.querySelector('.input-line')
-    // console.log(elInputLine.value ===)
     const color = elInputStrokeColor.value;
     if (checkChoosingStrokeColorBeforeOpeningLineData(elInputLine, color)) return
 
@@ -326,18 +302,11 @@ function checkChoosingStrokeColorBeforeOpeningLineData(elInput, strokeColor) {
 
 function onTextInput(text) {
 
-    console.log(gPickedColor)
-    console.log(gPickedAlign)
-    console.log(gPickedFont)
-
     let pickedColor = gPickedColor ? gPickedColor : 'black'
     let pickedAlign = gPickedAlign ? gPickedAlign : 'left'
     let pickedFont = gPickedFont ? gPickedFont : 35
     let pickedStrokeColor = gPickedStrokeColor ? gPickedStrokeColor : 'black'
-    console.log(pickedColor, pickedAlign)
-    // console.log(gCtx.color)
-    // if(gCtx.color !== 'black') console.log('diff black')
-    console.log(text.value)
+
     setLineTxt(text.value, pickedColor, pickedAlign, pickedFont, pickedStrokeColor)
     renderMeme()
 }
@@ -414,7 +383,7 @@ function switchLines(currMeme, lenOfLines, elInput) {
                 break
         }
     } else {
-        
+
         switch (currMeme.selectedLineIdx) {
             case 0:
                 gCtx.fillText(elInput.value, 30, arr[0])
@@ -454,7 +423,6 @@ function onAddTxtLine() {
 }
 
 function onDeleteLine() {
-    console.log('inside delete')
     const elInput = document.querySelector(".input-line")
     elInput.value = ''
 
@@ -545,16 +513,6 @@ function renderImg(img) {
     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
 }
 
-
-
-
-
-
-
-
-
-
-
 // Advance Feathures
 
 function getRandomMeme() {
@@ -580,7 +538,6 @@ function getRandomMeme() {
 /* Stickers */
 
 function addIcon(data) {
-    console.log('hi')
     const currMeme = getMeme()
     if (!currMeme.lines.length) {
         const elImage = new Image()
@@ -593,106 +550,9 @@ function addIcon(data) {
         const elImage = new Image()
         elImage.src = data.src
         const widthTxt = gCtx.measureText(gMeme.
-        lines[index].txt).width
+            lines[index].txt).width
         elImage.onload = () => {
             gCtx.drawImage(elImage, widthTxt + 60, 10, 50, 50)
         }
     }
 }
-
-// Switch lines
-// function onSwitchLines(data) {
-//     setLine(1)
-//     const currMeme = getMeme()
-//     // currMeme.lines.forEach(line => {
-//     //     console.log(line.txt)
-//     // });
-//     currMeme.lines[1].txt = 'hi!'
-//     renderMeme()
-//     data.focus()
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_canvas_textalign
-
-
-
-
-// var maxWidth = cnv1.width - 10;
-// var lineHeight = 23;
-// var x_pos = (cnv1.width - maxWidth) / 2;
-// var y_pos = 15;
-
-// function drawImageOnCanvas() {
-//     const elImage = new Image()
-
-//     const meme = getMeme()
-//     const idxImg = meme.selectedImgId
-//     const memeImage = getImgById(idxImg)
-//     console.log(memeImage)
-//     elImage.src = memeImage.url
-
-//     elImage.onload = () => {
-//         gCtx.drawImage(elImage, 0, 0, gElCanvas.width, gElCanvas.height)
-//         // drawLineOfText()
-//         const elInputLine = document.querySelector('.inputLine')
-//         elInputLine.onkeyup = function(){
-//             clearCanvas(gCtx)
-//             // addTextToCanvas(this.value, 50, 50, 400, 23)
-//             var maxWidth = 120;
-//             var x_pos = 50;
-//             var y_pos = 50;
-//             var lineHeight = 23;
-
-//             addLineToCanvas(this.value, x_pos , y_pos, maxWidth, lineHeight)
-//             setLineTxt(meme , this.value)
-//         }
-//     }
-// }
-
-// // fix that!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// function addLineToCanvas(text, x, y, maxWidth, lineHeight) {
-//     let words = text.split(' ')
-//     console.log(words)
-//     let line = ''
-//     const space = ' '
-
-//     console.log(gCtx.measureText(words).width)
-//     if (gCtx.measureText(words).width > 120) {
-//         console.log(y)
-//         line = '\n'
-//         y += '\n' + lineHeight
-//         console.log(y)
-//     }
-//     for (var i = 0; i < words.length; i++) {
-//         const textLine = words[i]
-//         //Check the width of the text, before writing it on the canvas:
-//         if (gCtx.measureText(textLine).width > 120) {
-//             console.log('yes!!!!!!!!!!!!!!!!!!!!')
-//             line = '\n' + words[i]
-//             console.log(line, "line")
-//             console.log(words[i], "words[i]")
-//             y += lineHeight
-//             console.log(y, "y")
-//         } else line += space + words[i]
-//     }
-//     gCtx.fillStyle = 'pink'
-//     gCtx.font = 'bold 17px arial'
-//     gCtx.fillText(line, x, y)
-// }
